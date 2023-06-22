f081b5f26ac5851c60be96caf6d29830d8f8a8f5
omrizil1 <34809539+omrizil1@users.noreply.github.com>
2023-06-22 14:15:00 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,149 @@
+const express = require('express');
+const axios = require('axios');
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+const cron = require('node-cron');
+const app = express();
+const port = 8000;
+
+let work = []
+let workedComplete = []
+const maxWorkers = 2
+let currentWorkers = 0
+let creatingWorker = false;
+AWS.config.update({ region: 'us-east-1' });
+const ec2 = new AWS.EC2();
+app.use(express.json());
+
+app.put('/enqueue', (req, res) => {
+    const iterations = req.query.iterations;
+    const buffer = req.body.buffer;
+    const id = uuidv4();
+    work.push({
+        id,
+        iterations,
+        buffer,
+        time : Date.now()
+    })
+
+    // Perform any necessary processing with the iterations and body data
+    console.log('Iterations:', iterations);
+    console.log('buffer:', buffer);
+
+    // Send a response indicating successful processing
+    res.send(id);
+});
+
+
+app.post('/pullCompleted', (req, res) => {
+    const { top } = req.query;
+    console.log('pullCompleted called with top:', top);
+
+    const results = workedComplete.splice(0, top);
+    return res.status(200).json(results);
+
+})
+
+app.post('/submitWork', (req, res) => {
+    const body = req.body;
+    console.log(`submitWork endpoint was called with id ${body.id}`)
+    workedComplete.push(body)
+
+    return res.status(201).json(body)
+
+})
+
+app.get('/giveWork', (req,res) => {
+    let currentWork = work.shift()
+    if (currentWork) {
+        console.log(`giveWork endpoint called giving the currentWork with id ${currentWork.id} to the worker `)
+    }
+    res.status(200).json(currentWork);
+})
+
+app.get('/freeWorker', (req, res) => {
+    if (currentWorkers > 0 ) {
+        currentWorkers = currentWorkers -1
+        res.status(200).json(1)
+    } else {
+        res.status(200).json(0)
+    }
+})
+
+app.listen(port, () => console.log(`Express app running on port ${port}!`));
+
+cron.schedule('*/10 * * * * *', async () => {
+    console.log("cron run")
+    if (work.length > 0) {
+        const firstWork = work[0];
+        const timeDifference = Date.now() - firstWork.time;
+
+        if (timeDifference > 20000 && currentWorkers < maxWorkers && !creatingWorker) { // 20 seconds = 20000 milliseconds
+            // Create a new worker
+            console.log(`Going to create a worker currentWorkers created by this machine is ${currentWorkers}`)
+            creatingWorker = true
+            await createWorker();
+            currentWorkers = currentWorkers + 1
+            console.log(`Worker created currentWorkers ${currentWorkers}`)
+        }
+    }
+});
+
+// Function to create a new worker
+async function createWorker() {
+    // Create a new EC2 instance
+
+    try {
+
+        const userData = `#!/bin/bash
+                          curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+                          sudo apt-get install -y nodejs
+                          git clone https://github.com/omrizil1/cloud_computing_hw2.git /home/ubuntu/your-app
+                          cd /home/ubuntu/your-app
+                          npm install
+                          touch logfile.log
+                          nohup node worker.js > ./logfile.log 2>&1 &`;
+
+        const userDataBase64 = Buffer.from(userData).toString('base64');
+        const keyPairName = "cloud-course";
+
+        const params = {
+            ImageId: 'ami-042e8287309f5df03', // Replace with the desired AMI ID
+            InstanceType: 't3.micro',
+            KeyName: keyPairName, // Replace with the name of your EC2 key pair
+            MinCount: 1,
+            MaxCount: 1,
+            UserData: userDataBase64,
+            IamInstanceProfile: {
+                Name: 'InstanceRole'
+            },
+            SecurityGroups: ['my-sg-cloud-course']
+        };
+
+        ec2.runInstances(params, (err, data) => {
+            if (err) {
+                console.log('Error creating worker:', err);
+            } else {
+                const instanceId = data.Instances[0].InstanceId;
+                console.log('Worker instance created:', instanceId);
+
+                // Wait until the instance is running and the app is accessible
+                ec2.waitFor('instanceRunning', {InstanceIds: [instanceId]}, (err, data) => {
+                    if (err) {
+                        console.log('Error waiting for instance to run:', err);
+                    } else {
+                        console.log('Worker instance is running');
+                        setTimeout(() => {
+                            creatingWorker = false
+                        },70000)
+                        // Check if the app is running by making an HTTP request
+                        const instancePublicIp = data.Reservations[0].Instances[0].PublicIpAddress;
+                        const appUrl = `http://${instancePublicIp}:8000`; // Replace with the actual app URL
+                    }
+                });
+            }
+        });
+    } catch (error) {
+        console.log("error",error)
+    }
+}
---
+++ worker.js
@@ -0,0 +1,153 @@
+const express = require('express');
+const axios = require('axios');
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+const cron = require('node-cron');
+const crypto = require('crypto');
+const app = express();
+const port = 8000;
+
+AWS.config.update({ region: 'us-east-1' });
+const ec2 = new AWS.EC2();
+app.use(express.json());
+let lastRun = Date.now()
+const instanceName1 ='first';
+const instanceName2 = 'second';
+let nodesIps = []
+let initIps = false
+let finishInit = false;
+let lockTerminateInstance = false;
+
+app.listen(port, () => console.log(`Express app running on port ${port}!`));
+
+
+function work(buffer, iterations) {
+    return new Promise((resolve, reject) => {
+        try {
+            let output = crypto.createHash('sha512').update(buffer).digest();
+            for (let i = 0; i < iterations - 1; i++) {
+                output = crypto.createHash('sha512').update(output).digest();
+            }
+            resolve(output);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+const getInstanceIpAddress = async (instanceName) => {
+    const params = {
+        Filters: [
+            {
+                Name: 'tag:Name',
+                Values: [instanceName]
+            }
+        ]
+    };
+
+    try {
+        console.log("getInstanceIpAddress " ,instanceName)
+        const response = await ec2.describeInstances(params).promise();
+        const instances = response.Reservations
+            .flatMap(reservation => reservation.Instances)
+            .filter(instance => instance.State.Name === 'running');
+        if (instances.length > 0) {
+            return instances[0].PublicIpAddress || instances[0].PrivateIpAddress;
+        } else {
+            console.log(`No instances found with the name '${instanceName}'.`);
+            return null;
+        }
+    } catch (error) {
+        console.log('Error retrieving instance details:', error);
+        return null;
+    }
+};
+
+async function getWork() {
+    if (Date.now() - lastRun > 90000 && !lockTerminateInstance) {
+        lockTerminateInstance = true
+        console.log("Start terminating process")
+       let instanceId = await getInstanceId()
+        console.log("instanceId", instanceId, typeof (instanceId))
+        const params = {
+            InstanceIds: [instanceId]
+        };
+        for (let ip of nodesIps) {
+            let response = await axios.get(`http://${ip}:8000/freeWorker`)
+            console.log(`freeWorker for ${ip} got response.data of ${response.data}`)
+            if (response.data > 0 ) {
+                break
+            }
+        }
+        try {
+            await ec2.terminateInstances(params).promise();
+            console.log(`Instance ${instanceId} terminated successfully.`);
+        } catch (error) {
+            console.error(`Error terminating instance ${instanceId}:`, error);
+        }
+    } else {
+        for (let ip of nodesIps) {
+            console.log("getting work for ip:" ,ip)
+            let workObjectResponse;
+            try {
+                workObjectResponse = await axios.get(`http://${ip}:8000/giveWork`);
+            } catch (error) {
+                console.log(error)
+                return
+            }
+            let workData = workObjectResponse.data
+            if (workData) {
+                console.log(`got work from ${ip} the work is ${workObjectResponse.data}`)
+                lastRun = Date.now()
+                let workData = workObjectResponse.data
+                work(workData.buffer, workData.iterations)
+                    .then(output => {
+                        console.log(`WorkData ${workData} Output is: ${output} for workData`)
+                        let result = {
+                            finalValue : output,
+                            id : workData.id
+                        }
+                        axios.post(`http://${ip}:8000/submitWork`, result)
+                            .then(response => {
+                                console.log('Result submitted successfully', response.data);
+                            })
+                            .catch(error => {
+                                console.log('Error submitting result:', error);
+                            });
+                    }).catch(error => {
+                    console.log('Error processing work:', error);
+                });
+            }
+        }
+    }
+}
+
+async function getInstanceId() {
+    try {
+        console.log("getInstanceId run")
+        const response = await axios.get('http://169.254.169.254/latest/meta-data/instance-id');
+        console.log("instanceId is ", response.data)
+        console.log("instanceId is type ", typeof(response.data))
+        return response.data;
+    } catch (error) {
+        console.log('Error retrieving instance ID:', error);
+    }
+}
+
+cron.schedule('*/8 * * * * *',  async() => {
+    if (!initIps)  {
+        initIps = true
+        console.log("Init ips")
+        let firstIp = await getInstanceIpAddress(instanceName1)
+        console.log("First Ip", firstIp)
+        let secondIp = await getInstanceIpAddress(instanceName2)
+        console.log("Second Ip", secondIp)
+        nodesIps.push(firstIp)
+        nodesIps.push(secondIp)
+        console.log("Finish Init")
+        finishInit = true
+    } if (finishInit) {
+        getWork().then(r => r)
+    }
+
+})
